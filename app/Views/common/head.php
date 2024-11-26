7b5aa9489546be91973c652e6f3c3781e2adfb09
Louis-Hagues <louis.hagues@gmail.com>
2024-11-26 13:04:55 +0100

    création onglet + rajout global css

--- app/Views/common/head.php
+++ app/Views/common/head.php
@@ -6,6 +6,7 @@
         <title>test</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" >
+        <link rel="stylesheet" href="/assets/css/global.css">
     </head>
     <body>
         <nav class="d-flex justify-content-between align-items-center p-3 bg bg-primary">
@@ -22,7 +23,7 @@
                     <a href="/#" class="text-light"><i class="bi bi-gear-fill fs-1"></i></a>
                 </div>
                 <div>
-                    <a href="/#" class="text-light"><i class="bi bi-door-closed fs-1 hover-text-danger"></i></a>
+                    <a href="/#" class="hover-danger text-light"><i class="bi bi-door-closed fs-1 hover-text-danger"></i></a>
                 </div>
             </div>
         </nav>
---
+++ app/Views/dashboard/component/case.php
@@ -0,0 +1,28 @@
+
+<div class="container mt-4">
+    <!-- Barre principale -->
+    <div class="task-bar d-flex align-items-center justify-content-between">
+        <div class="d-flex align-items-center">
+            <strong class="me-3"> <?php $nomTache ?> </strong>
+            <span> <?php $dateTache ?> </span>
+        </div>
+        <div class="d-flex align-items-center">
+            <!-- Icônes d\'édition et de suppression -->
+            <button class="icon-btn me-3"><i class="bi bi-pencil"></i></button>
+            <button class="icon-btn me-3"><i class="bi bi-trash"></i></button>
+            <!-- Bouton de dépliement -->
+            <button class="icon-btn" data-bs-toggle="collapse" data-bs-target="#task-details" aria-expanded="false">
+                <i class="bi bi-chevron-right"></i>
+            </button>
+        </div>
+    </div>
+
+    <!-- Contenu dépliable -->
+    <div id="task-details" class="collapse mt-2">
+        <div class="task-details">
+            <ul class="mb-0">
+                ' . <?php $commentaires ?>. '
+            </ul>
+        </div>
+    </div>
+</div> 
