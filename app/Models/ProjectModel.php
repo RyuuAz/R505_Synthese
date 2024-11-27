c89be6dcc380771b3b4aee7b0c07cb4d1764fc20
TomGoureau <tom.goureau@gmail.com>
2024-11-27 15:48:42 +0100

    modif des models

--- app/Models/ProjectModel.php
+++ app/Models/ProjectModel.php
@@ -9,10 +9,8 @@ class ProjectModel extends Model
     protected $table = 'project';
     protected $primaryKey = 'prj_id';
     protected $allowedFields = [
-        'usr_id',
         'title',
         'description',
-        'status',
         'prj_created_at',
         'prj_updated_at'
     ];
@@ -60,15 +58,18 @@ class ProjectModel extends Model
     }
 
     /**
-     * Récupère tous les projets d'un utilisateur
+     * Récupère tous les projets liés à un utilisateur via la table `project_user`
      * @param int $userId ID de l'utilisateur
      * @return array Liste des projets
      */
     public function getProjectsByUser(int $userId)
     {
-        return $this->where('usr_id', $userId)->findAll();
+        return $this->select('project.*')
+            ->join('project_user', 'project.prj_id = project_user.prj_id')
+            ->where('project_user.usr_id', $userId)
+            ->findAll();
     }
-
+    
     public function getProjectById(int $prjID)
     {
         return $this->where('prj_id', $prjID)->first();
