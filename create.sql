3e16e060421c02f8b94f17511b2affa3da38c8ff
TomGoureau <tom.goureau@gmail.com>
2024-11-27 15:22:50 +0100

    modif de la base de donnée

--- create.sql
+++ create.sql
@@ -35,6 +35,8 @@ CREATE TYPE notification_status AS ENUM ('pending', 'sent', 'failed');
 -- Table users
 CREATE TABLE users (
     usr_id SERIAL PRIMARY KEY,
+    nom VARCHAR(255) UNIQUE NOT NULL,
+    prenom VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     is_active BOOLEAN DEFAULT FALSE,
