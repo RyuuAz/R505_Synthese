37623b8ff4c4873ac79848ec809293c88af327cd
TomGoureau <tom.goureau@gmail.com>
2024-11-29 13:35:14 +0100

    maj db

--- create.sql
+++ create.sql
@@ -35,8 +35,8 @@ CREATE TYPE notification_status AS ENUM ('a_faire', 'en_cours', 'termine');
 -- Table users
 CREATE TABLE users (
     usr_id SERIAL PRIMARY KEY,
-    nom VARCHAR(255) UNIQUE NOT NULL,
-    prenom VARCHAR(255) UNIQUE NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     is_active BOOLEAN DEFAULT FALSE,
